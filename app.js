6003933cf0743d6d1ffdec4575b3051401d66561
hernanesterelles <112177315+hernanesterelles@users.noreply.github.com>
2022-11-27 23:59:56 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,174 +2,135 @@
 
 
 
- //variables
-let number
-let costo
-let tarjeta
- let carrito 
+let carrito 
 
-    if(localStorage.getItem('carrito')){
-        carrito = JSON.parse(localStorage.getItem('carrito'))
-    }
-    else{
-        carrito=[]
-    }
-    console.log('carrito:', carrito);
-    // selectores
-    const classcontainer = document.querySelector('#classcontainer') 
-    
-    
-   
-    
-    //funciones
-    const reservarHabitacionesAlCarrito = (e) => {
-        const idhabitacionElegida = e.target.getAttribute('data-id');
-        const habitacionCamaElegida = habitacionCama.find((habitacionCama) => habitacionCama.id == idhabitacionElegida)
-        carrito.push(habitacionCamaElegida)
-        console.log(carrito)
-        localStorage.setItem('carrito', JSON.stringify(carrito))
-    }
-    const renderizarhabitacion =() =>{
-        habitacionCama.forEach((habitacionCama) =>{
-            const nuevaCard = document.createElement('div')
-            nuevaCard.className = 'card'
-            nuevaCard.innerHTML =  `    
-                        <h3>Habitaciones</h3>
-                        <p>
-                        <img src="${habitacionCama.img}" alt="">
-                        <p class="vistas">${habitacionCama.habitacion} con vista al jardin</p>
-                        <p> precio por noche desde $ ${habitacionCama.precio}</p>
-                        <p class="destino">Playa del carmen,MEXICO</p>
-                        <buton class="buton" data-id= ${habitacionCama.id}>Reservar Habitacion</buton>
-                        `
-                        classcontainer.append(nuevaCard)        
+if(localStorage.getItem('carrito')){
+    carrito = JSON.parse(localStorage.getItem('carrito'))
+}
+else{
+    carrito=[]
+}
+console.log('carrito:', carrito);
+// selectores
+const classcontainer = document.querySelector('#classcontainer') 
+const verCarrito =document.getElementById('ver-carrito')
+const modalContainer = document.getElementById('modal-container')
+const cantidadCarrito = document.getElementById('cantidadCarrito')
+
+
+
+
+    relojArrays.forEach((relojArrays) =>{
+        const nuevaCard = document.createElement('div')
+        nuevaCard.className = 'card'
+        nuevaCard.innerHTML =  `    
+                    <h3>Reloj</h3>                    
+                    <img src="${relojArrays.img}" alt="">
+                    <p class="vistas">${relojArrays.reloj} </p>
+                    <p> Precio $ ${relojArrays.precio}</p>
+                                     
+                    `;
+                    classcontainer.append(nuevaCard) ;
+
+                    let comprar = document.createElement('button') 
+        comprar.innerText ='Comprar';
+        comprar.className = 'comprar'
+        nuevaCard.append(comprar)
+
+        comprar.addEventListener('click', () =>{
+            const repeat = carrito.some((repeatProduct) => repeatProduct.id === relojArrays.id)
+
+if(repeat) {
+    carrito.map((prod) => {
+        if(prod.id === relojArrays.id){
+            prod.cantidad ++;
+        }
+    })
+}else{
+            carrito.push({
+                id : relojArrays.id,
+                reloj :relojArrays.reloj,
+                img :relojArrays.img,
+                precio : relojArrays.precio,
+                cantidad :relojArrays.cantidad
+
             })
-            const buton = document.querySelectorAll('.buton') 
-            buton.forEach((button) =>{   
-                addEventListener('click', reservarHabitacionesAlCarrito)
-                     })
-                    
+            
+        }
+        console.log(carrito);
+        carritoCouter();
+        })
+             
+        })
+        //carrito
+         const pintarCarrito = () => {
+            modalContainer.innerHTML = '';
+            modalContainer.style.display = 'flex'
+            const modalHeader = document.createElement('div');
+            modalHeader.className = 'modal-header';
+            modalHeader.innerHTML =  `  
+            <h1 class ='modal-header-title'>Carrito </h1>
+            `
 
-    }
-   
-  
-   
+            modalContainer.append(modalHeader);
+            
+            const modalButton= document.createElement('h1');            
+            modalButton.className ='modal-header-button';
+            modalButton.innerText='x';
+            modalButton.addEventListener('click',() =>{
+                modalContainer.style.display = 'none'
+            }) ;
 
-  //ejecuciones
- renderizarhabitacion()
+            modalHeader.append(modalButton);
 
- alert( 'Bienvenidos a nuestra cadena de hoteles')
 
- 
- 
- 
- alert( 'elija una de nuestras habitaciones')
- 
- 
- let agregarHabitacion = prompt('elija una habitacion \n 0-simple $5000\n 1-doble $6000\n 2-triple $7500 \n 3-simple $15000\n 4-doble $16000\n 5-triple $17500')
+            carrito.forEach((relojArrays) => {
+                let carritoContent = document.createElement('div')
+                carritoContent.className ='modal-content'
+                carritoContent.innerHTML = ` 
+                <img src="${relojArrays.img}" alt="">
+                <p class="vistas">${relojArrays.reloj} </p>
+                <p> precio   $ ${relojArrays.precio}</p>
+                <p>cantidad ${relojArrays.cantidad}</p>
+                <p>Total : ${relojArrays.cantidad * relojArrays.precio}</p>
+                
+                ` 
+    
+                modalContainer.append(carritoContent)
+                console.log(carritoContent)
 
- 
- 
- 
- 
- 
- 
-   
- switch (agregarHabitacion) {
-     
-     case '0':
-         alert('Usted eligió una habitacion simple');
-         habitacion = ('simple');
-         number = prompt('¿Cuantos dias?');
-         costo = habitacion1.precio * number;
-         break;
-     case '1':
-         alert('Usted eligió una habitacion doble');
-         habitacion = ('doble');
-         number = prompt('¿Cuantos dias?');
-         costo = habitacion2.precio * number;
-         break;
-     case '2':
-         alert('Usted eligió una habitacion triple');
-         habitacion = ('triple');
-         number = prompt('¿Cuantos dias?');
-         costo = habitacion3.precio * number;
-         break;
-         case '3':
-            alert('Usted eligió una habitacion simple');
-            habitacion = ('junior');
-            number = prompt('¿Cuantos dias?');
-            costo = habitacion1.precio * number;
-            break;
-        case '4':
-            alert('Usted eligió una habitacion doble');
-            habitacion = ('suite');
-            number = prompt('¿Cuantos dias?');
-            costo = habitacion2.precio * number;
-            break;
-        case '5':
-            alert('Usted eligió una habitacion triple');
-            habitacion = ('familiar');
-            number = prompt('¿Cuantos dias?');
-            costo = habitacion3.precio * number;
-            break;
-     
-     default:
-         alert('Usted ha ingresado un valor incorrecto');
-         
-         break;
- }while (agregarHabitacion == false || agregarHabitacion>5 || agregarHabitacion<1);
-  {
-     alert(' usted se quedaria  '+ number +' dias '+ 'en una habitacion ' + habitacion + ' con un costo de $' + costo + ' Pesos') }
- 
- let datosTarjeta=0
- let valorCuota=0
- let cuotas=0
- 
- alert("total de la compra: $"+(costo))
- let pago= prompt("con que metodo desea pagar? tarjeta o transferencia")
- if(pago=="tarjeta"){
-     let debOcred=prompt("debito o credito?")
-     if(debOcred== "credito"){
-          cuotas = Number(prompt("cuantas cuotas? 3, 6 o 12"))
-         if(cuotas==3) {
- 
-          valorCuota= costo/cuotas
-          alert("3 cuotas de: $"+ valorCuota)
+                let eliminar = document.createElement('span')
+                eliminar.innerText = '❌'
+                eliminar.className = 'delete-product'
+                carritoContent.append(eliminar)
+
+                eliminar.addEventListener('click', eliminarProducto );
+            });
+
+            const total = carrito.reduce ((acc, el) => acc + el.precio * el.cantidad ,0);          
+            
+
+            const totalReloj = document.createElement('div')
+            totalReloj.className ='total-content'
+            totalReloj.innerHTML = `total a pagar: ${total} $ `;
+
+            modalContainer.append(totalReloj);
+            
+           }
+           verCarrito.addEventListener('click', pintarCarrito)
+           const eliminarProducto = ()=>{
+            const foundid = carrito.find((element) => element.id)
+
+            carrito = carrito.filter((carritoid) => {
+                return carritoid !== foundid;
+            })
+            carritoCouter()
+            pintarCarrito()
+           }
+        const carritoCouter = () =>{
+            cantidadCarrito.style.display = 'block';
+            cantidadCarrito.innerText = carrito.length;
+        };
         
-         }else if(cuotas==6){
-                  valorCuota= costo/cuotas
-          alert("6 cuotas de: $"+ valorCuota)
-         
-         }else if(cuotas ==12){
-                  valorCuota= costo/cuotas
-          alert("12 cuotas de: $"+ valorCuota)
-          alert('Gracias por su compra')
- 
-         
-         }else{
-             alert("opcion invalida")
-         }
-     }else if(debOcred=="debito"){
-          datosTarjeta=Number(prompt("ingrese los 12 digitos de su tarjeta"))
-          if(datosTarjeta>=100000000000 && datosTarjeta<=999999999999){
-             alert("tarjeta aprobada")
-             alert('Gracias por su compra')
- 
-          }else{
-             alert("tarjeta invalida")
-          }
-     }else {
-         alert("opcion invalida")
-     }
- }else if(pago=="transferencia"){
-     alert("transferir al cbu 878775656455334")
-     console.log("trasnferir al cbu 878775656455334")
-     alert('Gracias por su compra')
- 
-     }else{
-         alert("opcion incorrecta")
- 
-     }
-     
- 
+                
+
--- data.js
+++ data.js
@@ -1,27 +1,26 @@
-
-
-
-
-
-class habitaciones{
-    constructor(id,habitacion,img,precio){
+class relojes{
+    constructor(id,reloj,img,precio,cantidad){
         this.id=id;
-        this.habitacion=habitacion;
+        this.reloj=reloj;
         this.img=img;
         this.precio=precio;
+        this.cantidad=cantidad;
         
     }
 }
-const habitacion1=new habitaciones(001,' Habitacion simple',' https://bloghogar.com/wp-content/uploads/2011/01/23-fotos-decoracion-dormitorios-modernos-vinilo-600x450.jpg',5000)
-const habitacion2=new habitaciones(002,' Habitacion doble', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7IUuNw3ISM58EuOHmCNjcaxfZZsgNVeJXvA&usqp=CAU',6000)
-const habitacion3=new habitaciones(003,' Habitacion triple','  https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk01rc8oz04K9QI9z0iD5tpQo_85f9N21Bqg&usqp=CAU', 7500)
-const habitacion4=new habitaciones(001,' Habitacion junior',' https://bloghogar.com/wp-content/uploads/2011/01/23-fotos-decoracion-dormitorios-modernos-vinilo-600x450.jpg',15000)
-const habitacion5=new habitaciones(002,' Habitacion suite', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7IUuNw3ISM58EuOHmCNjcaxfZZsgNVeJXvA&usqp=CAU',16000)
-const habitacion6=new habitaciones(003,' Habitacion familiar','  https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk01rc8oz04K9QI9z0iD5tpQo_85f9N21Bqg&usqp=CAU', 17500)
+const reloj1=new relojes(01,' reloj',' https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxjlFw1cn_bFHMdNcWGNA2STPnXzpFbKTmfg&usqp=CAU',50000,1)
+const reloj2=new relojes(02,' reloj', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKwxwAsSuiOPXzH6pStOubZBzdqW3tTnN9ew&usqp=CAU',60000,1)
+const reloj3=new relojes(03,' reloj','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs4UTD0-eVt3NKk-H03if25Ahhbf2-KRLmPg&usqp=CAU  ', 70500,1)
+const reloj4=new relojes(04,' reloj',' https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9fja2g6PwZI3QGZ01u0o0IA28HNcZYJCw4w&usqp=CAU',15000,1)
+const reloj5=new relojes(05,' reloj', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcaX9exteB7kQNPFLlLwL6MYx659d-6M13lKX2mspsWp2qAmlk6_yu26BXn84a8W5whFM&usqp=CAU',16000,1)
+const reloj6=new relojes(06,' reloj','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpjwq4lhSdQw3rovEniKCoLDY_ok4MUuAGGA&usqp=CAU', 17500,1)
+const reloj7=new relojes(07,' reloj','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSm4-IFcf3ZNEZ3mWmBxacXesDiymi7CjWRpg&usqp=CAU ',50000,1)
+const reloj8=new relojes(08,' reloj', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKwxwAsSuiOPXzH6pStOubZBzdqW3tTnN9ew&usqp=CAU',60000,1)
+const reloj9=new relojes(09,' reloj','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd31G5S-0lAe5nsJbdTFT4veBTLCooEZ9URA&usqp=CAU ', 70500,1)
+const reloj10=new relojes(10,' reloj',' https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9fja2g6PwZI3QGZ01u0o0IA28HNcZYJCw4w&usqp=CAU',15000,1)
+const reloj11=new relojes(11,' reloj', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo0aVc31I1YTXM2wEXQAKv0zpV4pGpuMWQDQ&usqp=CAU',16000,1)
+const reloj12=new relojes(12,' reloj','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ17EspRaZC0_3W3fPXEPn4O2S82bA7hISNWg&usqp=CAU', 17500,1)
 
 
+const relojArrays=[reloj1,reloj2,reloj3,reloj4,reloj5,reloj6,reloj7,reloj8,reloj9,reloj10,reloj11,reloj12]
 
-const habitacionCama=[habitacion1,habitacion2,habitacion3,habitacion4,habitacion5,habitacion6]
-
-
-                
